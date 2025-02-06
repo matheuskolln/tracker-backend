9145df2cd6b3df10a3679cb104ae67ef40d0882d
Matheus Kolln <matheuzhenrik@gmail.com>
2025-02-06 05:26:06 -0300

    chore: update API routes for task time tracking

--- routes/api.php
+++ routes/api.php
@@ -3,6 +3,7 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TaskController;
+use App\Http\Controllers\TaskTimeController;
 use Illuminate\Http\Request;
 
 
@@ -23,4 +24,8 @@ Route::middleware('auth:sanctum')->prefix('tasks')->group(function () {
     Route::put('{task}', [TaskController::class, 'update']); // Update task
     Route::delete('{task}', [TaskController::class, 'destroy']); // Delete task
     Route::patch('{task}/status', [TaskController::class, 'changeStatus']); // Change task status
+    Route::post('{task}/start-timer', [TaskTimeController::class, 'startTimer']); // Start timer
+    Route::post('{task}/stop-timer', [TaskTimeController::class, 'stopTimer']); // Stop timer
+    Route::post('{task}/manual-time', [TaskTimeController::class, 'manualTimeEntry']); // Manual time entry
+    Route::get('{task}/times', [TaskTimeController::class, 'index']); // Get all task times
 });
